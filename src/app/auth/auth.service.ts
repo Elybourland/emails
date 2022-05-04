986f0f15e83ea35dbe03c9d9c72e80bcbb57e125
Elybourland <JEBourland@yahoo.com>
2022-05-04 13:57:08 -0700

    httpclient withCrendentials: true

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -37,8 +37,10 @@ export class AuthService {
   signup(credentials: SignupCredentials) {
     return this.http.post<SignupResponse>(
       `${this.rootUrl}/auth/signup`, 
-      credentials
-    ).pipe(
+      credentials, {
+        withCredentials: true
+      })
+      .pipe(
       tap(() => {
         this.signedin$.next(true);
       })
@@ -46,7 +48,9 @@ export class AuthService {
   }
 
   checkAuth() {
-    return this.http.get(`${this.rootUrl}/auth/signedin`)
+    return this.http.get(`${this.rootUrl}/auth/signedin`, {
+      withCredentials: true
+    })
     .pipe(
       tap(response => {
         console.log(response);
