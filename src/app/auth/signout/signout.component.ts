387ee1acaddf3842784af9c45915b76bf0f2da2a
Elybourland <JEBourland@yahoo.com>
2022-05-12 20:38:10 -0700

    navigate user to sign in after sign out

--- src/app/auth/signout/signout.component.ts
+++ src/app/auth/signout/signout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -8,11 +9,15 @@ import { AuthService } from '../auth.service';
 })
 export class SignoutComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.authService.signout().subscribe(() => {
       // Navigate user back to a sign in page
+      this.router.navigateByUrl('/');
     });
   }
 
