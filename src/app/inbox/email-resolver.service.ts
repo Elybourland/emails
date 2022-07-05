19a74c2750ec96ea95e7e3fa890e18f4dad91d13
Elybourland <JEBourland@yahoo.com>
2022-07-05 11:25:08 -0700

    using a resolver

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -10,6 +10,13 @@ export class EmailResolverService implements Resolve<Email> {
   constructor() { }
 
   resolve() {
-    
+    return {
+      id: 'adgadg',
+      subject: 'adsgadsg',
+      to: 'adgadg',
+      from: 'adgadsg',
+      text: 'asdgadg',
+      html: 'adsgadsg'
+    };
   }
 }
